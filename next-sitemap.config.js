0161dc9f9640f672b8c6c94f8127bbd5236be856
wadith027 <wadith@usf.edu>
2026-02-06 03:00:26 -0500

    SEO update

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://shadmanwadith.xyz', // Replace with your actual domain
+  generateRobotsTxt: true, // Creates a robots.txt file too
+  output: 'export', // Important for GitHub Pages compatibility
+};
